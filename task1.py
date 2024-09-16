db60a4a49c5a20b415210903e4ed03543edbbab0
kareevato <t.karieieva@gmail.com>
2024-09-16 22:41:27 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,22 @@
+## Лабораторна робота з дисципліни "Алгоритми та структури даних"
+
+## Виконала: Карєєва Тетяна Олександрівна (ІР-14)
+## Лабораторна робота №1 (Варіант 9)
+## Task №1
+
+# Змінна color та виведення її значення
+color = "Ваш улюблений колір"  # Замініть на ваш улюблений колір
+print("Ваш улюблений колір:", color)
+
+# Логічні операції
+is_voter = True
+is_non_citizen = False
+print("is_voter and is_non_citizen:", is_voter and is_non_citizen)
+print("is_voter or is_non_citizen:", is_voter or is_non_citizen)
+print("not is_voter:", not is_voter)
+print("not is_non_citizen:", not is_non_citizen)
+
+# Логічний оператор "або"
+is_raining = True
+is_snowing = True
+print("is_raining or is_snowing:", is_raining or is_snowing)
